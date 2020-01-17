7fadfa44db20875ce706cca8f0a6e6e051eb3dec
jacob biederman <jacob.biederman@mymail.champlain.edu>
2020-01-17 10:07:02 -0500

    started php page

---
+++ demo/index.php
@@ -0,0 +1,15 @@
+<?php
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First PHP Page</title>
+    <link rel="stylesheet" type="text/css" href="./common/styles.css"/>
+    <meta>
+</head>
+<body>
+<p>howdy</p>
+</body>
+</html>
+';
+?>
