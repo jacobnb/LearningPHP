2410cc20a68a45141d66bcbb795d6edfc936a840
jacob biederman <jacob.biederman@mymail.champlain.edu>
2020-01-17 10:47:00 -0500

    arrays and foreach loop

--- demo/index.php
+++ demo/index.php
@@ -1,15 +1,27 @@
 <?php
+include("./include/header_1.inc");
 echo'
-<!DOCTYPE html>
-<html lang="en">
-<head>
     <title>First PHP Page</title>
-    <link rel="stylesheet" type="text/css" href="./common/styles.css"/>
-    <meta>
-</head>
-<body>
-<p>howdy</p>
-</body>
-</html>
 ';
+include("./include/header_2.inc");
+/* A variable and array */
+$num = 5;
+$num2 = 10;
+$mult = $num*$num2;
+echo'
+<h1>The results of the operation = ' . $mult . '</h1>';
+$states = array('VT', 'NH', 'ME');
+$exams = array('exam1' => 89, 'exam2' => 90, 'exam3' => 100);
+echo'<h1>'.$states[0].'</h1>';
+print_r($states);
+// echo'<h1>'.$exams.'</h1>';
+echo'<table><tr><td>State Name</td><td>State Abbr.</td></tr>';
+    foreach ($states as $x) {
+        echo '<tr><td>State name: todo </td><td>'.$x.'</td></tr>';
+    }
+    echo'</table>';
+echo'
+<p>Howdy!</p>
+</div> <!-- id=container -->';
+include("./include/footer.inc");
 ?>
---
+++ demo/php/template.php
@@ -0,0 +1,19 @@
+<?php
+include("../include/header_1.inc");
+echo'
+    <title></title>
+';
+include("../include/header_2.inc");
+echo'
+<p>Howdy!</p>
+</div> <!-- id=container -->
+';
+
+
+
+
+
+
+
+include("../include/footer.inc");
+?>
