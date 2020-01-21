a37f928fd2d0b6952024004e7179ffa96a43c989
jacob <jacob.biederman@mymail.champlain.edu>
2020-01-21 10:42:30 -0500

    updated states table to use associative array

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,6 @@
 <?php
 include("./include/header_1.inc");
+// Title needs to be different from SEO
 echo'
     <title>First PHP Page</title>
 ';
@@ -11,17 +12,19 @@ $mult = $num*$num2;
 echo'
 <h1>The results of the operation = ' . $mult . '</h1>';
 $states = array('VT', 'NH', 'ME');
+$names = array('VT'=>'Vermont', 'NH'=>'New Hampshire', 'ME'=>'Maine');
+// associative array
 $exams = array('exam1' => 89, 'exam2' => 90, 'exam3' => 100);
 echo'<h1>'.$states[0].'</h1>';
 print_r($states);
 // echo'<h1>'.$exams.'</h1>';
 echo'<table><tr><td>State Name</td><td>State Abbr.</td></tr>';
     foreach ($states as $x) {
-        echo '<tr><td>State name: todo </td><td>'.$x.'</td></tr>';
+        echo '<tr><td>'.$names[$x].'</td><td>'.$x.'</td></tr>';
     }
-    echo'</table>';
+echo '</table>';
+   
 echo'
-<p>Howdy!</p>
 </div> <!-- id=container -->';
 include("./include/footer.inc");
 ?>
